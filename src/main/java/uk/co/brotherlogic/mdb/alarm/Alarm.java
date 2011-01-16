7f56fda2ad168706162a16e3ea3cfc48b33d5227
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2011-01-16 09:43:23 +0000

    Working on play command

--- src/main/java/uk/co/brotherlogic/mdb/alarm/Alarm.java
+++ src/main/java/uk/co/brotherlogic/mdb/alarm/Alarm.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Collections;
@@ -99,7 +100,9 @@ public class Alarm
             System.err.println(command);
             Process p = Runtime.getRuntime().exec(new String[]
             { command, toPlay.getAbsolutePath() });
-            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            for (String line = reader.readLine(); line != null; line = reader.readLine())
+               System.out.println(line);
          }
          catch (IOException e)
          {
